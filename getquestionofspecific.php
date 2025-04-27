ef2ebb6073634ce0d2bc7f34fe2c74caa0a2ec79
Reine-Chahine <reinechahine3@gmail.com>
2025-04-27 10:09:43 +0300

    adding questions array to get specfic question with while loop

--- getquestionofspecific.php
+++ getquestionofspecific.php
@@ -4,5 +4,10 @@ include 'connection.php';
 $quiz_id = $_GET['quiz_id'];
 
 $result = mysqli_query($conn, "SELECT * FROM questions WHERE quiz_id='$quiz_id'");
+$questions = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $questions[] = $row;
+}
 
+echo json_encode($questions);
 ?>
