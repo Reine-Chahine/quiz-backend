41b42878d7bc713076fb92efc96989aa31fa7d27
Reine-Chahine <reinechahine3@gmail.com>
2025-04-27 09:30:16 +0300

    adding sql insertion

--- register.php
+++ register.php
@@ -6,4 +6,12 @@ $username = $data['username'];
 $password = password_hash($data['password'], PASSWORD_DEFAULT);
 $email = $data['email'];
 
+$sql = "INSERT INTO Users (username, password, email, create_time, is_admin) 
+        VALUES ('$username', '$password', '$email', NOW(), 0)";
+
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(["message" => "User registered successfully."]);
+} else {
+    echo json_encode(["message" => "Registration failed."]);
+}
 ?>
