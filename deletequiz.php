5ce3fd78ee17954b806363e52bd90076dd2ef450
Reine-Chahine <reinechahine3@gmail.com>
2025-04-27 10:01:42 +0300

    update

--- deletequiz.php
+++ deletequiz.php
@@ -1,14 +1,15 @@
 <?php
 include 'connection.php';
 
-$data = json_decode(file_get_contents("php://input"), true);
-$ques_id = $data['ques_id'];
 
-$sql = "DELETE FROM questions WHERE ques_id='$ques_id'";
+$data = json_decode(file_get_contents("php://input"), true);
+$quiz_id = $data['quiz_id'];
+
+$sql = "DELETE FROM quizzes WHERE quiz_id='$quiz_id'";
 
 if (mysqli_query($conn, $sql)) {
-    echo json_encode(["message" => "Question deleted successfully"]);
+    echo json_encode(["message" => "Quiz deleted successfully"]);
 } else {
-    echo json_encode(["message" => "failed"]);
+    echo json_encode(["message" => "Quiz deletion failed"]);
 }
 ?>
