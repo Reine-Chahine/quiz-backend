dd9b0c11796f4c91eae75566ede6adf016d75202
Reine-Chahine <reinechahine3@gmail.com>
2025-04-27 10:14:25 +0300

    adding the sql attribute with if else to update or fail

--- editquestion.php
+++ editquestion.php
@@ -6,5 +6,13 @@ $ques_id = $data['ques_id'];
 $question_title = $data['question_title'];
 $type = $data['type'];
 $correct = $data['correct'];
+$sql = "UPDATE questions 
+        SET question_title='$question_title', type='$type', correct='$correct' 
+        WHERE ques_id='$ques_id'";
 
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(["message" => "Question updated successfully"]);
+} else {
+    echo json_encode(["message" => "Question update failed"]);
+}
 ?>
