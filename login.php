02be7558bafddcc959e96125cce39446e87780f6
Reine-Chahine <reinechahine3@gmail.com>
2025-04-27 09:33:10 +0300

    inserting sql

--- login.php
+++ login.php
@@ -4,4 +4,15 @@ include 'connection.php';
 $data = json_decode(file_get_contents("php://input"), true);
 $username = $data['username'];
 $password = $data['password'];
+$result = mysqli_query($conn, "SELECT * FROM Users WHERE username='$username'");
+
+if ($row = mysqli_fetch_assoc($result)) {
+    if (password_verify($password, $row['password'])) {
+        echo json_encode(["message" => "Login successful", "user_id" => $row['user_id']]);
+    } else {
+        echo json_encode(["message" => "Invalid password"]);
+    }
+} else {
+    echo json_encode(["message" => "User not found"]);
+}
 ?>
